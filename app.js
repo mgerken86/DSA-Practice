bc7667fda244a0724f9395d1b52b57df903b20ad
Mark Gerken <mgerken86@gmail.com>
2022-08-31 18:31:04 -0600

    linked list pop method

--- app.js
+++ app.js
@@ -2,33 +2,54 @@
 // *** LINKED LISTS ***
 
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value
         this.next = null
     }
 }
 
 class LinkedList {
-    constructor(value){
+    constructor(value) {
         const newNode = new Node(value)
         this.head = newNode
         this.tail = newNode
         this.length = 1
     }
 
-    push(value){
+    push(value) {
         const newNode = new Node(value)
-        if (!this.head){
+        if (!this.head) {
             this.head = newNode
             this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
         }
-        this.tail.next = newNode
-        this.tail = newNode
-        this.length += 1
+        this.length++
+        return this
     }
+
+    pop() {
+        if (!this.head) return undefined
+        let temp = this.head
+        let pre = this.head
+        while (temp.next) {
+            pre = temp
+            temp = temp.next
+        }
+        this.tail = pre
+        this.tail.next = null
+        this.length--
+        if (this.length === 0) {
+            this.head === null
+            this.tail === null
+        }
+        return temp
+    }
+
 
 }
 
-let myLinkedList = new LinkedList(4 )
+let myLinkedList = new LinkedList(4)
 myLinkedList.push(7)
 console.log(myLinkedList)

e10214ed2f3bb8f620e5ac724730d4ee1edb4c96
Mark Gerken <mgerken86@gmail.com>
2022-09-12 13:19:15 -0600

    DLL get method

--- app.js
+++ app.js
@@ -200,7 +200,7 @@ class DoubleLinkedList {
         return this
     }
 
-    shift(){
+    shift() {
         if (this.length === 0) return undefined
         let temp = this.head
         if (this.length === 1) {
@@ -212,6 +212,22 @@ class DoubleLinkedList {
             temp.next = null
         }
         this.length--
+        return temp
+    }
+
+    get(index) {
+        if (index < 0 || index > this.length) return undefined
+        let temp = this.head
+        if (index < this.length / 2) {
+            for (let i = 0; i < index; i++) {
+                temp = temp.next
+            }
+        } else {
+            temp = this.tail
+            for (let i = this.length - 1; i > index; i--) {
+                temp = temp.prev
+            }
+        }
         return temp
     }
 }

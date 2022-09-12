c2474374f9c3855fb8d71de5e468ad91665f65ad
Mark Gerken <mgerken86@gmail.com>
2022-09-12 13:15:25 -0600

    DLL shift method

--- app.js
+++ app.js
@@ -186,16 +186,33 @@ class DoubleLinkedList {
         return temp
     }
 
-    shift(value) {
+    unShift(value) {
         const newNode = DLLNode(value)
         if (!this.head) {
-            this.push(value)
+            this.head = newNode
+            this.tail = newNode
         } else {
+            newNode.next = this.head
             this.head.prev = newNode
             this.head = newNode
         }
         this.length++
         return this
+    }
+
+    shift(){
+        if (this.length === 0) return undefined
+        let temp = this.head
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.head = this.head.next
+            this.head.prev = null
+            temp.next = null
+        }
+        this.length--
+        return temp
     }
 }
 

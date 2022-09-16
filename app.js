cf61e3bc3f2704943c0e63b70a77da5d3e203523
Mark Gerken <mgerken86@gmail.com>
2022-09-15 20:32:21 -0600

    Stack push method

--- app.js
+++ app.js
@@ -283,4 +283,15 @@ class Stack {
         this.length = 1
     }
 
+    push(value) {
+        const newNode = new Node(value)
+        if (this.length === 0){
+            this.top = newNode
+        } else {
+            newNode.next = this.top
+            this.top = newNode
+        }
+        this.length++
+        return this
+    }
 }
